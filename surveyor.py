755c1253d18aa9596b0d0aa2935689cbd6e65e72
Jason Heron <jasonaheron@gmail.com>
2018-11-12 14:14:25 -0800

    update print statement

--- surveyor.py
+++ surveyor.py
@@ -20,6 +20,7 @@ def parse_wifi_map(map_path):
     print('*' * 40)
     for ssid in wifi_map:
         ssid_node = wifi_map[ssid]
+        print ssid_node
         print('ssid = {}, vendor = {}'.format(ssid, ssid_node['vendor']))
         for bssid in ssid_node:
             # print('\tbssid = {}'.format(bssid))
