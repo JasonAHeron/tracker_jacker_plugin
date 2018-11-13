ff7a0df659f3e57f3dc150522796c601b1e028cf
Jason Heron <jasonaheron@gmail.com>
2018-11-13 14:23:31 -0800

    kk

--- surveyor.py
+++ surveyor.py
@@ -1,10 +1,14 @@
-import sys
-import yaml
-import os
-import time
-import logging
-from watchdog.observers import Observer
+from firebase_admin import credentials
+from firebase_admin import firestore
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import firebase_admin
+import logging
+import os
+import sys
+import time
+import yaml
+
 
 def parse_wifi_map(map_path):
     with open(map_path, 'r') as f:
@@ -21,22 +25,35 @@ def parse_wifi_map(map_path):
     for ssid in wifi_map:
         ssid_node = wifi_map[ssid]
         print('ssid = {}'.format(ssid))
+        doc_ref = db.collection('networksa').document(ssid)
+        doc_ref.set({
+            'ssid': ssid,
+        })
         for bssid in ssid_node:
             # print('\tbssid = {}'.format(bssid))
             bssid_node = ssid_node[bssid]
             if 'devices' in bssid_node:
                 for device in bssid_node['devices']:
                     devices |= {device}
-                    print('\tdevice = {}, vendor = {}, last_seen = {} seconds ago'.format(device, bssid_node['devices'][device]['vendor'], time.time() - bssid_node['devices'][device]['last_seen']))
+                    print('\tdevice = {}, vendor = {}, last_seen = {} seconds ago'.format(
+                        device, bssid_node['devices'][device]['vendor'], time.time() - bssid_node['devices'][device]['last_seen']))
 
-    print('\n\nSSID count: {}, Device count: {}'.format(len(wifi_map), len(devices)))
+    print('\n\nSSID count: {}, Device count: {}'.format(
+        len(wifi_map), len(devices)))
+
 
 class Event(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path.endswith('wifi_map.yaml'):
             parse_wifi_map('wifi_map.yaml')
 
+
 if __name__ == "__main__":
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred, {
+        'projectId': project_id,
+    })
+    db = firestore.client()
     event_handler = Event()
     observer = Observer()
     observer.schedule(event_handler, '.', recursive=True)
